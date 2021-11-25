cb4c4fb08f555d647952ce435ace5c5be7d14ea2
eagledb14 <eagledb14@gmail.com>
2021-11-25 03:41:37 +0000

    fixed the home view

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -3,13 +3,11 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>{{ word }}</h1>
     <button v-on:click="getWord">Click</button>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import axios from "axios"
 
 export default {
